eb7b4eb03dd52595edff3f630f7c9713ee11dd88
Seba Gamboa <me@sagmor.com>
2014-06-06 13:39:26 -0400

    Parse dates on examples test

--- lib/ventana/method.rb
+++ lib/ventana/method.rb
@@ -1,5 +1,5 @@
 require "i18n"
 
 def Ventana(from,to,options={})
-  ""
+  "#{from} #{to}"
 end
--- test/examples_test.rb
+++ test/examples_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 require 'csv'
+require 'time'
 
+# This test case runs all the examples from the examples.csv file
 class ExampleTest < TestCase
 
   CSV.foreach(File.expand_path('../examples.csv',__FILE__),{
@@ -8,7 +10,7 @@ class ExampleTest < TestCase
   }).each_with_index do |row, index|
     define_method "test_example_#{index + 1}" do
       I18n.with_locale row['locale'] do
-        assert_equal row['expectation'], Ventana(row['from'], row['to'])
+        assert_equal row['expectation'], Ventana(Time.parse(row['from']), Time.parse(row['to']))
       end
     end
   end
